c35e3f75f9b145eab5a8c324e32e538882f2aaa5
M Sahil <74181152+msahilgit@users.noreply.github.com>
2023-11-10 09:47:44 +0530

    Create rcsb.sh

---
+++ rcsb.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+usage=$(echo $(tput bold)$(tput setaf 3)"Usage:   "$(tput setaf 6)"rcsb pdb_code name     ||  without '.pdb' extension"$(tput sgr0) )
+if [ "$1" = "" ]; then
+  echo $(tput bold)"NO pdb code is provided"
+  echo ${usage}
+  proceed=0
+else
+  pdb=`echo $1 | tr '[a-z]' '[A-Z]'`
+  proceed=1
+  if [ "$2" = "" ] ; then
+    name=$1
+  else
+    name=$2
+    if [ -f ${name}.pdb ] ; then
+      echo $(tput bold)$(tput setaf 1)'Warning:  '$(tput setaf 6)"file ${name}.pdb already present"
+    fi
+  fi
+fi
+#
+if [ ${proceed} -eq 0 ] ; then
+  exit 1
+fi
+#
+wget https://www.rcsb.org/pdb/files/${pdb}.pdb > /dev/null 2>&1 & 
+PID=$!
+bar='|/-\'
+echo -n $(tput bold)$(tput setaf 5)"Downloading $1  "$(tput sgr0)
+while [ -d /proc/$PID ]
+do
+  echo -ne $(tput bold)"\b"${bar:i++%${#bar}:1}
+  sleep 0.2
+done
+echo ' '
+#
+if [ -f ${pdb}.pdb ] ; then
+  mv ${pdb}.pdb ${name}.pdb
+  echo 'DONE' $(tput sgr0) 
+  ls ${name}.pdb
+else
+  echo "      "$(tput bold)$(tput setaf 1)"ERROR:: "$(tput setaf 6)"Structure ${pdb}.pdb not found at RCSB"$(tput sgr0)
+  echo ""
+  echo ${usage}
+  echo ""
+fi
+
+
+
+
+  #bar=("|" "/" "-" '\')
+  #p=0
+  #i=1
+  #while [ -d /proc/${PID} ] ; do
+    #j=`expr ${p} % 4`
+    #echo -ne "\b"${bar[$j]}
+    #echo -ne "\b"${bar:i++%${#bar}:1}
+    #sleep 0.2
+  #done
+  #wait
